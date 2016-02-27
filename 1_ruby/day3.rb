7d07a91ec1bde557c3a33f003f895c3cc7ff0489
renoth <johannes.renoth@gmx.de>
2016-02-27 22:44:45 +0100

    Ruby day 3

---
+++ 1_ruby/day3.rb
@@ -0,0 +1,66 @@
+module ActsAsCsv
+  class CsvRow
+    def initialize(csv, content)
+      @csv = csv
+      @contents = content
+    end
+
+    #catch missing method and access array of content in proper position
+    def method_missing(name, *args)
+      index = @csv.headers.index(name.to_s)
+      if index
+        @contents[index]
+      else
+        "!!incorrect accessor!!"
+      end
+    end
+  end
+
+  def self.included(base)
+    base.extend ClassMethods
+  end
+
+  module ClassMethods
+    def acts_as_csv
+      include InstanceMethods
+    end
+  end
+
+  module InstanceMethods
+
+    def read
+      @csv_contents = []
+      filename = self.class.to_s.downcase + '.txt'
+      file = File.new(filename)
+      @headers = file.gets.chomp.split(',')
+      file.each do |row|
+        @csv_contents << row.chomp.split(',')
+      end
+    end
+
+    def each(&block)
+      csv_contents.each do |contents|
+        block.call(CsvRow.new(self, contents))
+      end
+    end
+
+    attr_accessor :headers, :csv_contents
+
+    def initialize
+      read
+    end
+  end
+end
+
+class RubyCsv
+  include ActsAsCsv
+  acts_as_csv
+end
+
+m = RubyCsv.new
+
+puts m.headers.inspect
+puts m.csv_contents.inspect
+
+m.each {|row| puts row.name + ' is in ' + row.counry}
+m.each {|row| puts row.name + ' is in ' + row.country}
