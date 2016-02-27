6101d5bf224d961fda76301a3093e6c9a65f3d87
renoth <johannes.renoth@gmx.de>
2016-02-27 15:57:22 +0100

    first week

---
+++ 1_ruby/day1.rb
@@ -0,0 +1,11 @@
+puts "Hello World"
+
+index = "Hello World".index("World")
+puts index
+
+i = 0
+
+while (i < 10)
+  i = i + 1
+  puts "This is sentence number %d" % i
+end
---
+++ 1_ruby/day2.rb
@@ -0,0 +1,65 @@
+
+
+p '---------- 1 ---------'
+
+array = Array.new(17) {|i| 10 + i}
+
+array.each_index{|x| 
+  if x % 4 == 0
+    puts array[x..x+3].to_s
+  end
+}
+
+array.each_slice(4) {
+  |a| p a
+}
+
+p '---------- 2 ---------'
+
+class Tree
+  attr_accessor :children, :node_name
+
+  def initialize(name, hash = {})
+    @node_name = name
+
+    node_children = hash.keys
+    child_trees = []
+
+    node_children.each {
+      |key|
+      child_trees.push(Tree.new(key, hash.fetch(key)))
+    }
+
+    @children = child_trees
+  end
+
+  def visit_all(&block)
+    visit &block
+    children.each {|c| c.visit_all &block}
+  end
+
+  def visit(&block)
+    block.call self
+  end
+end
+
+hash = {'root' => {'level 1 1' => {'level 2 1' => {}, 'level 2 2' => {}}, 'level 1 2' => {}}}
+
+tree = Tree.new('root', hash.fetch(hash.keys[0]))
+
+tree.visit_all {|h| p h.node_name}
+
+p '---------- 3 ---------'
+
+lineNumber = 1
+
+IO.readlines(File.open("day2text.txt", "r")).each {
+  |line|
+  if (/fr/ =~ line)
+    p lineNumber.to_s + ' ' + line
+  end
+  lineNumber = lineNumber + 1
+}
+
+
+
