b197f6e7b55bc75cb9d9f1b4db73abfc2f26f5d4
renoth <johannes.renoth@gmx.de>
2016-07-25 16:24:39 +0200

    w4d1

--- 4_scala/day1.scala
+++ 4_scala/day1.scala
@@ -1,7 +1,11 @@
 import scala.collection.mutable
 
 class Game(board: List[String]) {
-  println("creating new game: ")
+
+  println("Checking game: ")
+  println(board(0))
+  println(board(1))
+  println(board(2))
 
   var lanes = board :::
   //add columns
@@ -11,13 +15,37 @@ class Game(board: List[String]) {
   //add diagonals
     List(board(0).split("")(0) + board(1).split("")(1) + board(2).split("")(2), board(0).split("")(2) + board(1).split("")(1) + board(2).split("")(0))
 
-  def hasWinner(): Unit = {
+  def laneComplete(value: String, isComplete: Boolean): Boolean = {
+    var occur = value.groupBy(_.toChar)
+    if (occur.size == 1 && !occur.keys.head.charValue().equals(" ".charAt(0).charValue())) {
+      return true
+    }
+    false
+  }
 
+  def hasWinner(): Unit = {
+    var hasWinner = false
+    var isComplete = false
+
+    for (i<- 0 until lanes.length) {
+      hasWinner |= laneComplete(lanes(i), hasWinner)
+      if (hasWinner) {
+        println("The Winner is '" + lanes(i).charAt(0) + "'")
+        return
+      }
+    }
+
+    if (lanes(0).contains(" ") || lanes(1).contains(" ") || lanes(2).contains(" ")) {
+      println("The game is incomplete")
+    } else {
+      println("the game is complete")
+    }
   }
 }
 
 object GameApp {
   def main(args: Array[String]): Unit = {
-    val game = new Game(List("XOX"," O ", " OX"))
+    val game = new Game(List("XOO","XXX", "XOO"))
+    game.hasWinner()
   }
 }
