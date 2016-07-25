b34e95cb4a0e0a2053dd387b11cd1cc93e2d599b
renoth <johannes.renoth@gmx.de>
2016-07-25 15:45:34 +0200

    w4d1

--- 4_scala/day1.scala
+++ 4_scala/day1.scala
@@ -1,12 +1,23 @@
+import scala.collection.mutable
+
 class Game(board: List[String]) {
-  println("hello")
-  val rows = board
+  println("creating new game: ")
+
+  var lanes = board :::
+  //add columns
+  List(board.lift(0).get.split("").lift(0).get + board.lift(1).get.split("").lift(0).get + board.lift(2).get.split("").lift(0).get,
+    board.lift(0).get.split("").lift(1).get + board.lift(1).get.split("").lift(1).get + board.lift(2).get.split("").lift(1).get,
+    board.lift(0).get.split("").lift(2).get + board.lift(1).get.split("").lift(2).get + board.lift(2).get.split("").lift(2).get) :::
+  //add diagonals
+    List(board(0).split("")(0) + board(1).split("")(1) + board(2).split("")(2), board(0).split("")(2) + board(1).split("")(1) + board(2).split("")(0))
+
+  def hasWinner(): Unit = {
+
+  }
 }
 
 object GameApp {
   def main(args: Array[String]): Unit = {
-    println("test")
     val game = new Game(List("XOX"," O ", " OX"))
-    println(game)
   }
 }
